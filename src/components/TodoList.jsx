541c76db8b2430fa83151fb9bab167145019283b
yuki <yuki.onishi1129@gmail.com>
2021-12-07 12:34:43 +0900

    update: delete function

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,61 +1,114 @@
+/**
+ * TodoList
+ * @package components
+ */
 import React, { useState } from "react";
+/* constants */
+import { INITIAL_TODO_LIST, INITIAL_TODO_LIST_COUNT } from "../constants/data";
 
+/**
+ * TodoList
+ * @returns
+ */
 export const TodoList = () => {
-  const initialState = [
-    {
-      task: "Task1",
-      isCompleted: false,
-    },
-    {
-      task: "Task2",
-      isCompleted: false,
-    },
-  ];
+  /* state */
+  /* todo list */
+  const [todos, setTodos] = useState(INITIAL_TODO_LIST);
+  /* add input title */
+  const [addInputValue, setAddInputValue] = useState("");
+  /* todo 採番ID */
+  const [uniqueId, setUniqueId] = React.useState(INITIAL_TODO_LIST_COUNT);
 
-  const [todos, setTodo] = useState(initialState);
-  const [task, setTask] = useState("");
-
-  const handleNewTask = (event) => {
-    setTask(event.target.value);
+  /* actions */
+  /**
+   * addInputValueの変更処理
+   * @param {*} event
+   */
+  const handleChangeAddInputValue = (event) => {
+    setAddInputValue(event.target.value);
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (task === "") return;
-    setTodo((todos) => [...todos, { task, isCompleted: false }]);
-    setTask("");
+  /**
+   * タスク追加処理
+   * @param {*} event
+   */
+  const handleAddTodo = (event) => {
+    //  エンターキーが押された時にTodoを追加する
+    if (event.key === "Enter" && addInputValue !== "") {
+      const nextUniqueId = uniqueId + 1;
+      // Todo追加処理
+      // 元の配列を破壊しないように配列のコピーを作成して、その値でstateを更新する
+      // pushでの配列追加は元の配列の値を変更するのでエラーになる
+
+      // concatの処理
+      // setTodos(
+      //   // concatとpushの違い
+      //   // https://kskpblog.com/javascript-array-add/
+      //   todos.concat({
+      //     id: nextUniqueId,
+      //     title: addInputValue,
+      //   })
+      // );
+
+      // スプレッド構文の処理
+      setTodos([
+        ...todos,
+        {
+          id: nextUniqueId,
+          title: addInputValue,
+        },
+      ]);
+      // 採番IDを更新
+      setUniqueId(nextUniqueId);
+      // todo追加後、入力値をリセット
+      setAddInputValue("");
+    }
   };
 
-  const handleRemoveTask = (index) => {
-    const newTodos = [...todos];
-    if (window.confirm("todoを削除してもいいですか？")) {
-      newTodos.splice(index, 1);
-      setTodo(newTodos);
+  /**
+   * タスク削除処理
+   * @param {*} targetId
+   * @param {*} targetTitle
+   */
+  const handleRemoveTask = (targetId, targetTitle) => {
+    if (window.confirm(`「${targetTitle}」のtodoを削除しますか？`)) {
+      // 削除するid以外のtodoリストを再編集
+      // filterを用いた方法
+      const newTodos = todos.filter((todo) => todo.id !== targetId);
+      // 削除するTodoの配列番号を取り出してspliceで削除する方法もある
+      // const newTodos = [...todoList];
+      // const deleteIndex = newTodos.findIndex((todo) => todo.id === targetId);
+      // newTodos.splice(deleteIndex, 1);
+      setTodos(newTodos);
     }
   };
 
   return (
     <div className="wrapper">
       <h1>Todo List</h1>
-      <h2>ADD TASK</h2>
+
       <section className="common-area">
-        <form onSubmit={handleSubmit}>
+        <h2>ADD TASK</h2>
+        {/* formタグでsubmitはおすすめされない */}
+        {/* https://document.intra-mart.jp/library/iap/public/im_ui/im_design_guideline_pc/texts/contents_area/submit.html */}
+        <div>
           <input
             type="text"
-            value={task}
+            value={addInputValue}
             placeholder="New Task"
-            onChange={handleNewTask}
+            onChange={handleChangeAddInputValue}
+            onKeyDown={handleAddTodo}
           />
-        </form>
+        </div>
       </section>
       <section className="common-area">
         <ul className="todolist">
-          {todos.map((todo, index) => (
-            <li className="todo" key={index}>
-              <span className="todo-task">{todo.task}</span>
+          {todos.map((todo) => (
+            <li className="todo" key={todo.id}>
+              <span className="todo-task">{todo.title}</span>
               <i
                 className="far fa-trash-alt"
-                onClick={() => handleRemoveTask(index)}
+                onClick={() => handleRemoveTask(todo.id, todo.title)}
               ></i>
             </li>
           ))}
---
+++ src/constants/data.js
@@ -0,0 +1,25 @@
+/**
+ * data
+ * @package constants
+ */
+
+/**
+ * Todoリスト初期値
+ */
+export const INITIAL_TODO_LIST = [
+  {
+    id: 1,
+    title: "Task1",
+    isCompleted: false,
+  },
+  {
+    id: 2,
+    title: "Task2",
+    isCompleted: false,
+  },
+];
+
+/**
+ * Todoリスト数の初期値
+ */
+export const INITIAL_TODO_LIST_COUNT = INITIAL_TODO_LIST.length;
